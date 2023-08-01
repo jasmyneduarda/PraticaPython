c4e2d48c6fe02b6edcf2a9a21bd409b89ba47699
Jasmyn Eduarda <jasmyn.sabino@aluno.senai.br>
2023-08-01 11:12:03 -0300

    fixação

---
+++ PraticaPython/ex_fixacao01.py
@@ -0,0 +1,9 @@
+'''
+Fazer um programa que pergunte um valor em Dólares e converta pra Reais. Considere que 1 Dólar vale 4.78 reais
+'''
+
+print ("Para Converter o Dólar em Real")
+num = float(input("Coloque aqui o seu valor em Dólar: "))
+
+R = num * 4.78
+print ("Em Reais: ", R)
